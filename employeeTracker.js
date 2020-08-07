a64e82ebaa99e43769bbdc797e8b85840193bdde
Tarmity Flassman <tarmity.flassman@gmail.com>
2020-08-07 12:42:23 +1000

    added the first function with the initial Questions

--- employeeTracker.js
+++ employeeTracker.js
@@ -1,6 +1,8 @@
+// Varibles =====================================================
 const mysql = require("mysql");
-const inquirer = require("inquirer"); 
+const inquirer = require("inquirer");
 
+// Establish connection with MySQL ==============================
 const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -9,7 +11,50 @@ const connection = mysql.createConnection({
     database: "employee_DB"
 });
 
-connection.connect(function(err){
+connection.connect(function (err) {
     if (err) throw err;
-    console.log("successful");
+    //console.log("successful");
+    generalQuestion();
 })
+
+// Function that asks the inital Question =======================
+function generalQuestion() {
+    inquirer
+        .prompt({
+            name: "action",
+            type: "list",
+            message: "What would you like to do?",
+            choices: [
+                "Add Employee",
+                "Add Departments",
+                "Add Role",
+                "View Employees",
+                "View Departments",
+                "View Roles",
+                "Update Employee Roles"
+            ]
+        })
+
+// Then promise with a switch statement to follow on for the inital Question
+
+}
+
+
+// Function to Add Employee
+
+
+// Function to Add Departments
+
+
+// Function to Add Role
+
+
+// Function to View Employees
+
+
+// Function to View Departments
+
+
+// Function to View Roles
+
+// Function to Update Employee Roles
