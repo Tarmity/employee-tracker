9e0665c082100764a9373b8e36bcc25f0dbc0b6c
Tarmity Flassman <tarmity.flassman@gmail.com>
2020-08-10 12:33:32 +1000

    Add the function to view Employee Company details

--- employeeTracker.js
+++ employeeTracker.js
@@ -3,21 +3,20 @@ const mysql = require("mysql");
 const inquirer = require("inquirer");
 const fs = require("fs");
 
-
 // Establish connection with MySQL ===========================================================
 const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "",
-    database: "employee_DB"
+    password: "Boston@29",
+    database: "employee_DB",
 });
 
-connection.connect(err => {
+connection.connect((err) => {
     if (err) throw err;
     console.log("Connected as ID " + connection.threadId);
     init();
-})
+});
 
 // Function that asks the inital Question ====================================================
 function init() {
@@ -27,22 +26,22 @@ function init() {
             type: "list",
             message: "What would you like to do?",
             choices: [
-                "View Employees", 
-                "View Departments", 
-                "View Roles",   
-                "Add Employee",
-                "Add Departments", 
-                "Add Role", 
+                "Veiw Employee/Company Details",
+                "Add Employee/Company Information",
                 "Update Employee Roles",
-                "Exit Application" 
-            ]
+                "Exit Application",
+            ],
         })
 
         // Then promise with a switch statement to follow on for the inital Question
         .then(function (answer) {
             switch (answer.action) {
-                case "Add Employee":
-                    addEmployee();
+                case "Veiw Employee/Company Details":
+                    viewEmployeeDetails();
+                    break;
+
+                case "Add Employee/Company Information":
+                    addInformation();
                     break;
 
                 case "Add Departments":
@@ -52,6 +51,34 @@ function init() {
                 case "Add Role":
                     addRole(); //Function Done
                     break;
+
+                case "Update Employee Roles":
+                    updateRoles();
+                    break;
+
+                case "Exit Application":
+                    process.exit(); //Function Done
+                    break;
+            }
+        });
+}
+
+// Function to View Employee Details =============================================================
+
+function viewEmployeeDetails() {
+    inquirer
+    .prompt({
+        name: "action",
+        type: "list",
+        message: "Please select what you would like to view",
+        choices: [
+            "View Employees",
+            "View Departments",
+            "View Roles",
+        ],
+    })
+        .then(function (answer) {
+            switch (answer.action) {
 
                 case "View Employees":
                     viewEmployees(); //Function Done
@@ -64,48 +91,40 @@ function init() {
                 case "View Roles":
                     viewRole(); //Function Done
                     break;
-
-                case "Update Employee Roles":
-                    updateRoles();
-                    break;
-
-                case "Exit Application":
-                    process.exit(); //Function Done
-                    break;
-
             }
-        })
+        });
 }
+
+// Function to add Employee/Company Information 
 
 // Function to Add Employee ======================================================================
 function addEmployee() {
-    // // query the role table
-    // let roleQuery = `SELECT * FROM employee_db.role;`;
-    // // exe -- give you a list of roles
-    // connection.query(roleQuery, function (err, res) {
-    //     for (let i = 0; i < res.length; i++) {
-    //         let title = res[i].title
-    //         function getArray(item){
-    //             let jobTitle = [title].join(" ");
-    //             let newTitle = jobtitle.map(getArray);
-    //         }
-    //         // for each role, grab the title, and turn them into an array (use map function )
-    //     }
-
-    // })
-    // once you have the title array, chuck it down there
+    // query the role table
+    let roleQuery = `SELECT * FROM employee_db.role;`;
+    // exe -- give you a list of roles
+    connection.query(roleQuery, function (err, res) {
+        for (let i = 0; i < res.length; i++) {
+            let title = res[i].title;
+            function getArray(item) {
+                let jobTitle = [title].join(" ");
+                let newTitle = jobtitle.map(getArray);
+            }
+            // for each role, grab the title, and turn them into an array (use map function )
+        }
+    });
+    //once you have the title array, chuck it down there
 
     inquirer
         .prompt([
             {
                 type: "input",
                 message: "please enter new employee's first name",
-                name: "first_name"
+                name: "first_name",
             },
             {
                 type: "input",
                 message: "Please Enter new employee's last name",
-                name: "last_name"
+                name: "last_name",
             },
             {
                 name: "role",
@@ -116,28 +135,20 @@ function addEmployee() {
                     "Manager",
                     "Software Engineer",
                     "Full Stack Developer",
-                    "Junior Full Stack Developer"   // need this to be a title Array 
-                ]
+                    "Junior Full Stack Developer", // need this to be a title Array
+                ],
             },
         ])
 
         .then(function (answer) {
-
             // const role = answer.role;
-
             // // query for the id of the selected role
             // let roleQuery = `SELECT * from \`role\` WHERE \`title\` = ${role}`
-
             // // exe the query
-
             // // grab the result, and get the id -- now you have the role id ready to insert to your create employee statement
-
             // let query = `INSERT INTO \`employee_db\`.\`employee\` (\`first_name\`, \`last_name\`, \`role_id\`, \`manager_id\`) VALUES ('${answer.first_name}', '${answer.last.name}', '${role}', '3');`
-
-        })
+        });
 }
-
-
 
 // // Function to Add Departments =============================================================================
 function addDepartments() {
@@ -145,24 +156,28 @@ function addDepartments() {
         .prompt([
             {
                 type: "input",
-                message: "What is the new Deparments ID number? (please do not use an ID already taken) ",
-                name: "id"
+                message:
+                    "What is the new Deparments ID number? (please do not use an ID already taken) ",
+                name: "id",
             },
             {
                 type: "input",
                 message: "What is your new Department called?",
-                name: "name"
+                name: "name",
             },
         ])
         .then(function (answer) {
             const sqlAddDepartment = `INSERT INTO department (id, name) VALUES ( ? , ?)`;
-            connection.query(sqlAddDepartment, [answer.id, answer.name], (err, res) => {
-                if (err) throw err;
-                console.log("Successful");
-                init()
-            })
-        })
-
+            connection.query(
+                sqlAddDepartment,
+                [answer.id, answer.name],
+                (err, res) => {
+                    if (err) throw err;
+                    console.log("Successful");
+                    init();
+                }
+            );
+        });
 }
 
 // // Function to Add Role =======================================================================================
@@ -171,34 +186,38 @@ function addRole() {
         .prompt([
             {
                 type: "input",
-                message: "What is the new Deparments ID number? (please do not use an ID already taken) ",
-                name: "id"
+                message:
+                    "What is the new Deparments ID number? (please do not use an ID already taken) ",
+                name: "id",
             },
             {
                 type: "input",
                 message: "What is the Title for the new role going to be?",
-                name: "title"
+                name: "title",
             },
             {
                 type: "input",
                 message: "What is the salary for the new role?",
-                name: "salary"
+                name: "salary",
             },
             {
                 type: "input",
                 message: "What is your new Department id number for the new role?",
-                name: "department_id"
+                name: "department_id",
             },
         ])
         .then(function (answer) {
             const sqlAddRole = `INSERT INTO role (id, title, salary, department_id ) VALUES ( ?, ?, ?, ?)`;
-            connection.query(sqlAddRole, [answer.id, answer.title, answer.salary, answer.department_id], (err, res) => {
-                if (err) throw err;
-                console.log("Successful");
-                init()
-            })
-        })
-
+            connection.query(
+                sqlAddRole,
+                [answer.id, answer.title, answer.salary, answer.department_id],
+                (err, res) => {
+                    if (err) throw err;
+                    console.log("Successful");
+                    init();
+                }
+            );
+        });
 }
 
 // Function to View all Employees =================================================================================
@@ -208,9 +227,8 @@ function viewEmployees() {
         if (err) throw err;
         console.table(res);
         init();
-    })
+    });
 }
-
 
 // Function to View all Departments ===============================================================================
 function viewDepartments() {
@@ -219,12 +237,9 @@ function viewDepartments() {
         if (err) throw err;
         console.table(res);
         init();
-    })
-
+    });
 }
 // Function to view by individual Departments
-
-
 
 // // Function to View all Roles ===================================================================================
 function viewRole() {
@@ -233,13 +248,10 @@ function viewRole() {
         if (err) throw err;
         console.table(res);
         init();
-    })
-
+    });
 }
 
 //Function to Veiw by individual roles ==============================================================================
-
-
 
 // // Function to Update Employee Roles =============================================================================
 // function updateRoles(){
